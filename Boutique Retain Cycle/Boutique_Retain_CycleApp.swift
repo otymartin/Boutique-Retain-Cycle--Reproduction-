3b0d13661ef4854abe387aff32519b12727cd265
MBO <otyeka.martin@gmail.com>
2024-04-18 12:33:19 -0400

    Initial Commit

---
+++ Boutique Retain Cycle/Boutique_Retain_CycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boutique_Retain_CycleApp.swift
+//  Boutique Retain Cycle
+//
+//  Created by MBO on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Boutique_Retain_CycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boutique Retain Cycle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Boutique Retain Cycle
+//
+//  Created by MBO on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
